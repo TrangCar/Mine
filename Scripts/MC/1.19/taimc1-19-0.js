69ab31599163bfe2bf7617af732d9969c61de056
TrangCar <ngovantrang2722006@gmail.com>
2026-03-24 21:05:32 +0700

    fix mobi8

--- Scripts/MC/1.19/taimc1-19-0.js
+++ Scripts/MC/1.19/taimc1-19-0.js
@@ -6,45 +6,45 @@ let isDragging = false;
 let startX;
 let scrollLeft;
 
-// ===== PC (Mouse) =====
-slider.addEventListener("mousedown", (e) => {
-    isDragging = true;
-    slider.classList.add("active");
+// // ===== PC (Mouse) =====
+// slider.addEventListener("mousedown", (e) => {
+//     isDragging = true;
+//     slider.classList.add("active");
 
-    startX = e.pageX;
-    scrollLeft = slider.scrollLeft;
-});
+//     startX = e.pageX;
+//     scrollLeft = slider.scrollLeft;
+// });
 
-window.addEventListener("mouseup", () => {
-    isDragging = false;
-});
+// window.addEventListener("mouseup", () => {
+//     isDragging = false;
+// });
 
-slider.addEventListener("mouseleave", () => {
-    isDragging = false;
-});
+// slider.addEventListener("mouseleave", () => {
+//     isDragging = false;
+// });
 
-slider.addEventListener("mousemove", (e) => {
-    if (!isDragging) return;
+// slider.addEventListener("mousemove", (e) => {
+//     if (!isDragging) return;
 
-    e.preventDefault();
+//     e.preventDefault();
 
-    const x = e.pageX;
-    const walk = (x - startX) * 1.5; // chỉnh tốc độ kéo
-    slider.scrollLeft = scrollLeft - walk;
-});
+//     const x = e.pageX;
+//     const walk = (x - startX) * 1.5; // chỉnh tốc độ kéo
+//     slider.scrollLeft = scrollLeft - walk;
+// });
 
 
-// ===== Mobile (Touch) =====
-slider.addEventListener("touchstart", (e) => {
-    startX = e.touches[0].pageX;
-    scrollLeft = slider.scrollLeft;
-});
+// // ===== Mobile (Touch) =====
+// slider.addEventListener("touchstart", (e) => {
+//     startX = e.touches[0].pageX;
+//     scrollLeft = slider.scrollLeft;
+// });
 
-slider.addEventListener("touchmove", (e) => {
-    const x = e.touches[0].pageX;
-    const walk = (x - startX) * 1.5;
-    slider.scrollLeft = scrollLeft - walk;
-});
+// slider.addEventListener("touchmove", (e) => {
+//     const x = e.touches[0].pageX;
+//     const walk = (x - startX) * 1.5;
+//     slider.scrollLeft = scrollLeft - walk;
+// });
 
 // quay lại
 function goBack() {
