08b5b8df49989f2d76010878f1002b06f7a029d6
TrangCar <ngovantrang2722006@gmail.com>
2026-03-25 13:55:58 +0700

    Toi uu

--- Detail.js
+++ Detail.js
@@ -8,6 +8,6 @@ function goBack() {
         window.history.back();
     } else {
         // nếu mở trực tiếp → chuyển về index
-        window.location.href = "../../../index.html";
+        window.location.href = "index.html";
     }
 }
