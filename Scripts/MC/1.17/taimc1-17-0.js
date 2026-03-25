0136b243306b3f5a6f4f51fa43aa00468d85c010
TrangCar <ngovantrang2722006@gmail.com>
2026-03-25 12:53:45 +0700

    fix mobi14

--- Scripts/MC/1.17/taimc1-17-0.js
+++ Scripts/MC/1.17/taimc1-17-0.js
@@ -2,50 +2,6 @@
 
 const slider = document.getElementById("contentImgs");
 
-let isDragging = false;
-let startX;
-let scrollLeft;
-
-// ===== PC (Mouse) =====
-slider.addEventListener("mousedown", (e) => {
-    isDragging = true;
-    slider.classList.add("active");
-
-    startX = e.pageX;
-    scrollLeft = slider.scrollLeft;
-});
-
-window.addEventListener("mouseup", () => {
-    isDragging = false;
-});
-
-slider.addEventListener("mouseleave", () => {
-    isDragging = false;
-});
-
-slider.addEventListener("mousemove", (e) => {
-    if (!isDragging) return;
-
-    e.preventDefault();
-
-    const x = e.pageX;
-    const walk = (x - startX) * 1.5; // chỉnh tốc độ kéo
-    slider.scrollLeft = scrollLeft - walk;
-});
-
-
-// ===== Mobile (Touch) =====
-slider.addEventListener("touchstart", (e) => {
-    startX = e.touches[0].pageX;
-    scrollLeft = slider.scrollLeft;
-});
-
-slider.addEventListener("touchmove", (e) => {
-    const x = e.touches[0].pageX;
-    const walk = (x - startX) * 1.5;
-    slider.scrollLeft = scrollLeft - walk;
-});
-
 // quay lại
 function goBack() {
     if (sessionStorage.getItem("fromIndex")) {
