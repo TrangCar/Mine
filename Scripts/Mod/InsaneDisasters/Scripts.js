584d55c0cb8701faccd60b48ba87ba53cc1e5923
TrangCar <ngovantrang2722006@gmail.com>
2026-04-10 16:52:03 +0700

    ThemADS

---
+++ Scripts/Mod/InsaneDisasters/Scripts.js
@@ -0,0 +1,11 @@
+/* Trang.html */
+    function handleDownload() {
+        window.open("TaiXuong.html", "_blank");
+    }
+
+/* TaiXuong.html */
+    function downloadFile() {
+
+        // link tải
+        window.open("https://github.com/TrangCar/KhoModMC/releases/download/InsaneDisasters/Insane.Disasters.mcaddon","_blank");
+    }
---
+++ Scripts/Mod/ModTiengViet/Scripts.js
@@ -0,0 +1,11 @@
+/* Trang.html */
+    function handleDownload() {
+        window.open("TaiXuong.html", "_blank");
+    }
+
+/* TaiXuong.html */
+    function downloadFile() {
+
+        // link tải
+        window.open("https://www.mediafire.com/file/nuz7kg9jjkzcatf/Ty-el_Vietnamese_v1.6.6.mcpack/file","_blank");
+    }
